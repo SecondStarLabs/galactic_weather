850eee47ff05cb146ff3a1ea5faf7508ad643170
captproton <carl@wdwhub.net>
2020-04-14 15:00:53 -0700

    CHANGE: improved the styling and animation of martian_places/show.html.erb

--- app/controllers/mars_controller.rb
+++ app/controllers/mars_controller.rb
@@ -1,6 +1,6 @@
 class MarsController < ApplicationController
   def index
-    @latest_reading = @city.earthly_readings.last
-    @latest_seven_readings = @city.earthly_readings.last(7)    
+    # @latest_reading = @city.earthly_readings.last
+    # @latest_seven_readings = @city.earthly_readings.last(7)    
   end
 end
