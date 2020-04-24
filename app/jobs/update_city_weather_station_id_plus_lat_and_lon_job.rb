e6ccfe361cdea3c8416fd858ae8c45694866c64c
captproton <carl@wdwhub.net>
2020-04-23 19:07:32 -0700

    CHANGE: updated cities show and new pages to make them actually work

--- app/jobs/update_city_weather_station_id_plus_lat_and_lon_job.rb
+++ app/jobs/update_city_weather_station_id_plus_lat_and_lon_job.rb
@@ -11,38 +11,46 @@ class UpdateCityWeatherStationIdPlusLatAndLonJob < ApplicationJob
     # relationship
 
     @city = City.find(id)
-    ec = EarthlyCityGeoCollection.new(lat: @city.latitude, lon: @city.longitude)
-    weather_station = ec.get_first_weather_station_in_listed_radius
+    ews   = @city.earthly_weather_stations.first_or_create
 
-    ews                   = EarthlyWeatherStation.where(open_weather_id: weather_station.id).first_or_create(name: weather_station.name)
-    ews.country           = weather_station.country
-    ews.open_weather_id   = weather_station.id
-    # to guard against nil we coerce to float before to decimal
-    ews.lat               = weather_station.coord.lat.to_f.to_d
-    ews.lon               = weather_station.coord.lon.to_f.to_d
-    ews.save
+    if ews.name.to_s.length < 1
+      #find and create weather station if it doesn't exist
+      ec = EarthlyCityGeoCollection.new(lat: @city.latitude, lon: @city.longitude)
+      weather_station = ec.get_first_weather_station_in_listed_radius
 
-    reading = EarthlyReading.where(earthly_weather_station: ews).first_or_initialize
 
-    # test if this is a new record
-    # dt is a kind of timestamp
-    # so check to see if the dt is different
-    if reading.dt != weather_station.dt
-      reading.city                        = @city
-      reading.temp                        = weather_station.main.temp.to_d
-      reading.feels_like                  = weather_station.main.feels_like.to_d
-      reading.temp_min                    = weather_station.main.temp_min.to_d
-      reading.temp_max                    = weather_station.main.temp_max.to_d
-      reading.pressure                    = weather_station.main.pressure
-      reading.humidity                    = weather_station.main.humidity.to_d
-      reading.wind_speed                  = weather_station.wind.speed.to_d
-      reading.wind_deg                    = weather_station.wind.deg.to_d
-      reading.cloud_coverage_all          = weather_station.clouds.all
-      reading.dt                          = weather_station.dt
-      reading.recorded_at                 = Time.at(weather_station.dt)
+      ews.name              = weather_station.name
+      ews.country           = weather_station.sys.country
+      ews.open_weather_id   = weather_station.id
+      # to guard against nil we coerce to float before to decimal
+      ews.lat               = weather_station.coord.lat.to_f.to_d
+      ews.lon               = weather_station.coord.lon.to_f.to_d
+      ews.save
+    end
 
-      reading.save
+    if @city.earthly_readings.where(dt: weather_station.dt).length == 0
       
+      reading = @city.earthly_readings.new(earthly_weather_station: ews)
+      reading.temp = weather_station.main.temp.to_d
+      reading.feels_like = weather_station.main.feels_like.to_d
+      reading.temp_min = weather_station.main.temp_min.to_d
+      reading.temp_max = weather_station.main.temp_max.to_d
+      reading.pressure = weather_station.main.pressure
+      reading.humidity = weather_station.main.humidity.to_d
+      reading.wind_speed = weather_station.wind.speed.to_d
+      reading.wind_deg = weather_station.wind.deg.to_d
+      reading.cloud_coverage_all = weather_station.clouds.all
+      reading.dt = weather_station.dt
+      reading.recorded_at = Time.at(weather_station.dt)
+
+      # guard for saving an invalid record with nil attributes
+      if reading.recorded_at.to_s.length > 0
+        reading.save!
+      end
+
+      # FIX/REPLACE:  hack to temporarily fix saving record before attributes are set
+      # and then saving another record with valid attributes
+      @city.earthly_readings.where(recorded_at: nil).delete_all
     end
   end
 end
--- app/models/earthly_reading.rb
+++ app/models/earthly_reading.rb
@@ -1,4 +1,18 @@
 class EarthlyReading < ApplicationRecord
   belongs_to :earthly_weather_station
   belongs_to :city
+
+  def collect_all_current_readings
+    collected_open_weather_ids  = EarthlyWeatherStation.all
+                                  .collect {|earthly_weather_station|  earthly_weather_station.open_weather_id }
+    current_readings            = WeatherEarthly.new
+                                  .get_current_weather_for_a_list_of_open_weather_ids(collected_open_weather_ids)
+    representations             = current_readings.fetch("list")
+                                  .collect {|current_reading| we.create_representation(current_reading)}
+
+  end
+
+  def save_all_current_readings
+      
+  end
 end
