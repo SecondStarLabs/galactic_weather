9d3ec773c6371e33e3d708315b847bede54baf86
captproton <carl@wdwhub.net>
2020-08-29 14:19:08 -0700

    CHANGE: bundle update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
@@ -60,4 +62,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # hosts for development
+  config.hosts << 'cbt.ngrok.io'
 end
